ca1264c555b97c3475fa93a98467cce92829003d
Gayan <iamgayan@gmail.com>
2015-06-23 17:20:38 +0530

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+    <h3>I'am {{ $first }} {{ $last }}</h3>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque mollis gravida. Suspendisse id leo in odio tempus mollis. Curabitur rhoncus cursus nisl nec consequat. Curabitur hendrerit mauris eu faucibus dignissim. Pellentesque vitae arcu eleifend, laoreet lorem interdum, tempor dolor. Nulla diam enim, porta sit amet massa in, tempus auctor dolor. Phasellus tristique hendrerit justo, auctor consequat arcu sagittis vitae. Nulla vitae pellentesque lorem. Cras sed nibh ante. Vivamus non turpis ante. Vestibulum est arcu, convallis ac risus ac, bibendum luctus turpis. Aliquam mattis dignissim dui non fringilla.</p>
+@stop
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+    <h3>Contact Me!</h3>
+@stop
